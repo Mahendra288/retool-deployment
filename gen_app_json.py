531b503cdcb02a4427973da8a9a598b1d095c754
venkata.bandaru <venkata.bandaru@ibhubs.co.in>
2023-10-21 15:53:12 +0530

    organize json files

--- gen_app_json.py
+++ gen_app_json.py
@@ -8,8 +8,8 @@ from constants import EnvironmentEnum
 class GenerateAppJson:
 
     def generate_app_json(self, envs: List[EnvironmentEnum]):
-        env_config = self.read_json('env_config.json')
-        app_config = self.read_json('app.json')
+        env_config = self.read_json('config_jsons/env_config.json')
+        app_config = self.read_json('config_jsons/app.json')
 
         envs = envs if envs else [each.value for each in EnvironmentEnum]
         for env in envs:
@@ -18,7 +18,7 @@ class GenerateAppJson:
             for pattern in replace_patterns:
                 app_state_str = app_state_str.replace(pattern['from'], pattern['to'])
             app_config['page']['data']['appState'] = app_state_str
-            new_app_config_file_name = f"{env}-app.json"
+            new_app_config_file_name = f"app_jsons/{env}-app.json"
             self.write_json(new_app_config_file_name, app_config)
             print(f"Generated {new_app_config_file_name}")
 
