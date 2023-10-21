7701624ebd09809905ac08ac3c934e73312863d6
venkata.bandaru <venkata.bandaru@ibhubs.co.in>
2023-10-21 17:11:45 +0530

    push app jsons to s3

--- gen_app_json.py
+++ gen_app_json.py
@@ -2,7 +2,7 @@
 import json
 from typing import Dict, List
 
-from constants import EnvironmentEnum
+from constants.enums import EnvironmentEnum
 
 
 class GenerateAppJson:
@@ -18,9 +18,13 @@ class GenerateAppJson:
             for pattern in replace_patterns:
                 app_state_str = app_state_str.replace(pattern['from'], pattern['to'])
             app_config['page']['data']['appState'] = app_state_str
-            new_app_config_file_name = f"app_jsons/{env}-app.json"
-            self.write_json(new_app_config_file_name, app_config)
-            print(f"Generated {new_app_config_file_name}")
+            new_app_config_file_path = f"app_jsons/{env}-app.json"
+            self.write_json(new_app_config_file_path, app_config)
+            self._push_app_config_to_s3(
+                env=env,
+                local_file_path=new_app_config_file_path
+            )
+            print(f"Generated {new_app_config_file_path}")
 
     @staticmethod
     def read_json(file_path: str):
@@ -32,6 +36,18 @@ class GenerateAppJson:
     def write_json(file_path: str, app_config: Dict):
         with open(file_path, 'w') as json_file:
             json.dump(app_config, json_file, indent=4)
+
+    @staticmethod
+    def _push_app_config_to_s3(env: EnvironmentEnum, local_file_path: str):
+        from s3_service import S3Service
+        s3_service = S3Service()
+
+        file_contents = open(local_file_path, "rb").read()
+        # noinspection PyTypeChecker
+        s3_service.put_object(
+            body=file_contents,
+            file_name=f"alpha/media/retool-app-jsons/{env}-app.json"
+        )
 
 
 if __name__ == "__main__":
